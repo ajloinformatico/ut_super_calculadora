f8cb91b47bc0832fb58c4e1957a130329d2b9917
ajloinformatico <57002879+ajloinformatico@users.noreply.github.com>
2020-04-22 10:26:26 +0200

    Add files via upload

---
+++ supercalculadora/ut_calculadora.py
@@ -0,0 +1,51 @@
+import unittest
+import calculadora
+
+
+class TestCalculadora(unittest.TestCase):
+    def setUp(self):
+        self.calc = calculadora.Calculadora()
+
+    def tearDown(self):
+        pass
+
+    def test_sumar_2_y_2(self):
+        self.assertEqual(4, self.calc.sumar(2, 2))
+
+    def test_sumar_5_y_7(self):
+        self.assertEqual(12, self.calc.sumar(5, 7))
+
+    def test_sumar_propiedad_conmutativa(self):
+        self.assertEqual(self.calc.sumar(5, 7),
+                         self.calc.sumar(7, 5))
+
+    def test_restar_5_y_3(self):
+        self.assertEqual(2, self.calc.restar(5, 3))
+
+    def test_restar_2_y_3(self):
+        self.assertEqual(-1, self.calc.restar(2, 3))
+
+    def test_restar_no_propiedad_conmutativa(self):
+        self.assertNotEqual(self.calc.restar(5, 3),
+                            self.calc.restar(3, 5))
+
+    def test_sumar_numeros_negativos(self):
+        self.assertEqual(0, self.calc.sumar(2, -2))
+
+    def test_restar_numeros_negativos(self):
+        self.assertEqual(-7, self.calc.restar(-5, 2))
+        self.assertEqual(-5, self.calc.restar(-7, -2))
+
+    def test_division_exacta(self):
+        self.assertEqual(1, self.calc.dividir(2, 2))
+        self.assertEqual(2, self.calc.dividir(10, 5))
+
+    def test_division_exacta_negativa(self):
+        self.assertEqual(-2, self.calc.dividir(10, -5))
+        self.assertEqual(2, self.calc.dividir(-10, -5))
+
+    def test_division_no_entera_da_excepcion(self):
+        self.assertEqual(ValueError, self.calc.dividir(3, 2))
+
+    def test_division_por_0(self):
+        self.assertEqual(ZeroDivisionError, self.calc.dividir(3, 0))
---
+++ supercalculadora/ut_supercalculadora.py
@@ -0,0 +1,54 @@
+import unittest
+import supercalculadora
+import expr_aritmetica
+import ut_calculadora
+import ut_expr_aritmetica
+
+
+# class TestsSupercalculadora(unittest.TestCase):
+#     def test_sumar(self):
+#         sc = supercalculadora.Supercalculadora(
+#             expr_aritmetica.ExprAritmetica())
+#         self.assertEqual("4", sc.calcular("2 + 2"))
+
+#     def test_restar(self):
+#         sc = supercalculadora.Supercalculadora(
+#             expr_aritmetica.ExprAritmetica())
+#         self.assertEqual("0", sc.calcular("2 - 2"))
+
+#     def test_expresion_compleja_sin_parentesis_sin_precedencia(self):
+#         sc = supercalculadora.Supercalculadora(
+#             expr_aritmetica.ExprAritmetica())
+#         self.assertEqual("6", sc.calcular("5 + 4 - 3"))
+
+#     def test_expresion_compleja_sin_parentesis_con_precedencia(self):
+#         sc = supercalculadora.Supercalculadora(
+#             expr_aritmetica.ExprAritmetica())
+#         self.assertEqual("3", sc.calcular("5 + 4 / 2 - 4"))
+
+
+class TestsSupercalculadora(unittest.TestCase):
+    def setUp(self):
+        "añadido setup y teardown debajo"
+        self.sc = supercalculadora.Supercalculadora(
+            expr_aritmetica.ExprAritmetica())
+
+    def tearDown(self):
+        pass
+
+    def test_sumar(self):
+        self.assertEqual("4", self.sc.calcular("2 + 2"))
+
+    def test_restar(self):
+        self.assertEqual("0", self.sc.calcular("2 - 2"))
+
+    def test_expresion_compleja_sin_parentesis_sin_precedencia(self):
+        self.assertEqual("6", self.sc.calcular("5 + 4 - 3"))
+
+# añade función de simplificar
+    def test_expresion_compleja_sin_parentesis_con_precedencia(self):
+        self.assertEqual("3", self.sc.calcular("5 + 4 / 2 - 4"))
+        self.assertEqual("-1", self.sc.calcular("4 / 2 - 3"))
+        self.assertEqual("1", self.sc.calcular("4 / 2 - 3 + 1 + 6 / 3 - 1"))
+        self.assertEqual(
+            "-8", self.sc.calcular("4 / -2 + 3 + -1 + -6 / -3 - 10"))
